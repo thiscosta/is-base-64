88da28908154144fad03a17c12767cccc8edea1d
Thiago Costa <thiagocosta@Thiagos-MacBook-Pro.local>
2022-08-18 19:19:06 -0300

    Setup Package

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+export const isBase64 = (str: any): boolean => {
+  const notBase64 = /[^A-Z0-9+\/=]/i;
+  const isString = typeof str === 'string' || str instanceof String;
+
+  if (!isString) {
+    let invalidType;
+    if (str === null) {
+      invalidType = 'null';
+    } else {
+      invalidType = typeof str;
+      if (invalidType === 'object' && str.constructor && str.constructor.hasOwnProperty('name')) {
+        invalidType = str.constructor.name;
+      } else {
+        invalidType = `a ${invalidType}`;
+      }
+    }
+    throw new TypeError(`Expected string but received ${invalidType}.`);
+  }
+
+  const len = str.length;
+  if (!len || len % 4 !== 0 || notBase64.test(str.toString())) {
+    return false;
+  }
+  const firstPaddingChar = str.indexOf('=');
+  return (
+    firstPaddingChar === -1 || firstPaddingChar === len - 1 || (firstPaddingChar === len - 2 && str[len - 1] === '=')
+  );
+};
